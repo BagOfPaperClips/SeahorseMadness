5e3061f270b473079aa2c6a1c49619baa8fd0ae8
BagOfPaperClips <samrgenovese@gmail.com>
2025-09-08 22:34:54 -0400

    Menu
    I added the menu

---
+++ Assets/Scripts/SceneTransitioner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
