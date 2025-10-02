3f9dfb6175fd251c3f3673a7f31fea91abdef5b5
samoc01 <samoc@my.yorku.ca>
2025-10-01 23:01:12 -0400

    Working Vignette!
    Click to reset the vignette

---
+++ Assets/Scripts/VignetteController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class VignetteController : MonoBehaviour
+{
+    public float intensity = 0;
+
+    PostProcessVolume _volume;
+    Vignette _vignette;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volume = GetComponent<PostProcessVolume>();
+
+        _volume.profile.TryGetSettings<Vignette>(out _vignette);
+
+        if (!_vignette)
+        {
+            print("error, vignette empty");
+        }
+        else
+        {
+            _vignette.enabled.Override(true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intensity += 0.0005f;
+        if (intensity > 1f) intensity = 1f;
+        _vignette.intensity.Override(intensity);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            intensity = 0f;
+            _vignette.intensity.Override(0f);
+        }
+            
+    }
+}
