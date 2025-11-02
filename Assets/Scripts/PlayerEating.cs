3beb5b5ef05decb369c64c81418d8b0a54977475
samoc01 <samoc@my.yorku.ca>
2025-11-02 04:27:26 -0500

    Functioning vignette and reg death scene switching
    Vignette works again, and after the timer is done, the scene will switch

--- Assets/Scripts/PlayerEating.cs
+++ Assets/Scripts/PlayerEating.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+//ausing UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
 
 public class PlayerEating : MonoBehaviour
@@ -10,9 +12,8 @@ public class PlayerEating : MonoBehaviour
     public GameObject PPGO;
     public float intensity = 0;
 
-    PostProcessVolume _volume;
-    Vignette _vignette;
-    Bloom _bloom;
+    UnityEngine.Rendering.Universal.Vignette vignette;
+    UnityEngine.Rendering.Universal.Bloom bloom;
 
     //sound
     public AudioSource eatSound;
@@ -20,7 +21,8 @@ public class PlayerEating : MonoBehaviour
 
     //endscreen countdown (hidden to players)
     private bool _isEating = false;
-    public float timeRemaining = 4;
+    private float _elapsed = 0f;
+    public float timeRemaining = 5;
     public bool timerIsRunning = false;
 
     // Start is called before the first frame update
@@ -33,40 +35,35 @@ public class PlayerEating : MonoBehaviour
 
     void Start()
     {
-        _volume = PPGO.GetComponent<PostProcessVolume>();
+        //volume = PPGO.GetComponent<Volume>();
 
-        _volume.profile.TryGetSettings<Vignette>(out _vignette);
-        _volume.profile.TryGetSettings<Bloom>(out _bloom);
+        UnityEngine.Rendering.VolumeProfile volumeProfile = PPGO.GetComponent<UnityEngine.Rendering.Volume>()?.profile;
 
-        if (!_vignette)
+        volumeProfile.TryGet<Vignette>(out vignette);
+        volumeProfile.TryGet<Bloom>(out bloom);
+
+        if (!vignette)
         {
             print("error, vignette empty");
         }
-        else
-        {
-            _vignette.enabled.Override(true);
-        }
 
-        if (!_bloom)
+        if (!bloom)
         {
             print("error, bloom empty");
-        }
-        else
-        {
-            _bloom.enabled.Override(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        _elapsed += Time.deltaTime;
         if (_isEating == false)
         {
-            intensity += 0.0007f ;
+            intensity = 0.05f * _elapsed;
         }
         else
         {
-            intensity -= 0.035f;
+            intensity = 1 - (0.35f * _elapsed);
         }
         //max vignette sizes
         if (intensity > 1f) intensity = 1f;
@@ -77,8 +74,8 @@ public class PlayerEating : MonoBehaviour
             intensity = 0f;
         }
         //change vignette and bloom
-        _vignette.intensity.Override(intensity);
-        _bloom.intensity.Override(intensity*20);
+        vignette.intensity.Override(intensity);
+        bloom.intensity.Override(intensity*20);
 
         if (intensity == 1f) {
             timerIsRunning = true;
@@ -95,13 +92,13 @@ public class PlayerEating : MonoBehaviour
                 Debug.Log("Time has run out! You died of starvation");
                 timeRemaining = 0;
                 timerIsRunning = false;
-                //go to endscreen
+                SceneManager.LoadScene("DeathScreen");
             }
         }
         //if (Input.GetMouseButtonDown(0))
         //{
         //    intensity = 0f;
-        //    _vignette.intensity.Override(0f);
+        //    vignette.intensity.Override(0f);
         //}
     }
 
@@ -111,7 +108,7 @@ public class PlayerEating : MonoBehaviour
         {
             Debug.Log("Food collected: ");
             //intensity = 0f;
-            //_vignette.intensity.Override(0f);
+            //vignette.intensity.Override(0f);
             eatSound.Play();
             timerIsRunning = false;
             timeRemaining = 5;
